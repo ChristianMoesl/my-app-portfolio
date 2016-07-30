c2468435c3b06238d91784bb18253968b16fd8c7
Christian Mösl <moesl0r@hotmail.com>
2016-07-30 18:14:33 +0200

    Added a intent to start the first app.

--- app/src/main/java/at/mchris/myappportfolio/MainActivity.java
+++ app/src/main/java/at/mchris/myappportfolio/MainActivity.java
@@ -1,19 +1,26 @@
 package at.mchris.myappportfolio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     private Toast toast;
+
+    private HashMap<String, String> activites = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activites.put(getString(R.string.first_app_name), "at.mchris.popularmovies");
     }
 
     public void onAppStartRequested(View view) {
@@ -22,11 +29,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         final Button button = (Button)findViewById(view.getId());
-        String msg = "This Button will launch my "
-                + button.getText().toString().toLowerCase()
-                + " app!";
 
-        toast = Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT);
-        toast.show();
+        String packageName = activites.get(button.getText());
+
+        if (packageName != null) {
+
+            Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
+            startActivity(intent);
+
+        } else {
+            final String msg = "This Button will launch my "
+                    + button.getText().toString().toLowerCase()
+                    + " app!";
+
+            toast = Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
